44ab122700d3412fc79a442aa34770ca571066c9
RAnsah2022 <reb.o.ansah@gmail.com>
2022-06-20 22:06:26 -0400

    Jenkins New Script

---
+++ Jenkins-Install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##Author - Rebecca Ansah - reb.o.ansah@gmail.com
+
+##Date - June 2022
+
+#Jenkins Installation
+
+#1 - Install Java
+
+sudo yum install java-1.8.0-openjdk-devel -y
+
+#2 - Enable Jenkins Repositorysudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+sudo yum install wget -y
+
+sudo sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
+
+#3 - Install Latest Version of Jenkins
+
+sudo yum install jenkins -y
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+sudo systemctl enable Jenkins
+
+
+#4 - Adjust Firewall
+
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+
+sudo firewall-cmd --reload
+
